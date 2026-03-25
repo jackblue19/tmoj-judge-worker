3f91ceb54dcf29dd44977e03a8f3a3ce6e8f0040
Jack Blue <git.jack1624@gmail.com>
2026-03-25 09:01:57 +0700

    chore: scaffold for problems

--- src/Domain/Entities/RolePermission.cs
+++ src/Domain/Entities/RolePermission.cs
@@ -1,31 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using Microsoft.EntityFrameworkCore;
 
 namespace Domain.Entities;
 
-[PrimaryKey("RoleId", "PermissionId")]
-[Table("role_permission")]
 public partial class RolePermission
 {
-    [Key]
-    [Column("role_id")]
     public Guid RoleId { get; set; }
 
-    [Key]
-    [Column("permission_id")]
     public Guid PermissionId { get; set; }
 
-    [Column("status")]
     public bool Status { get; set; }
 
-    [ForeignKey("PermissionId")]
-    [InverseProperty("RolePermissions")]
     public virtual Permission Permission { get; set; } = null!;
 
-    [ForeignKey("RoleId")]
-    [InverseProperty("RolePermissions")]
     public virtual Role Role { get; set; } = null!;
 }
--- src/Domain/Entities/Runtime.cs
+++ src/Domain/Entities/Runtime.cs
@@ -1,42 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using Microsoft.EntityFrameworkCore;
 
 namespace Domain.Entities;
 
-[Table("runtime")]
 public partial class Runtime
 {
-    [Key]
-    [Column("id")]
     public Guid Id { get; set; }
 
-    [Column("runtime_name")]
     public string RuntimeName { get; set; } = null!;
 
-    [Column("runtime_version")]
     public string? RuntimeVersion { get; set; }
 
-    [Column("image_ref")]
     public string? ImageRef { get; set; }
 
-    [Column("default_time_limit_ms")]
     public int DefaultTimeLimitMs { get; set; }
 
-    [Column("default_memory_limit_kb")]
     public int DefaultMemoryLimitKb { get; set; }
 
-    [Column("is_active")]
     public bool IsActive { get; set; }
 
-    [InverseProperty("Runtime")]
     public virtual ICollection<JudgeRun> JudgeRuns { get; set; } = new List<JudgeRun>();
 
-    [InverseProperty("Runtime")]
     public virtual ICollection<Solution> Solutions { get; set; } = new List<Solution>();
 
-    [InverseProperty("Runtime")]
     public virtual ICollection<Submission> Submissions { get; set; } = new List<Submission>();
 }

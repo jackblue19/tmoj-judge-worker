c8513c08af8bd62912cb67016aedb982b902d71d
Jack Blue <git.jack1624@gmail.com>
2026-03-30 14:42:50 +0700

    chore: internal api test connection to judge-server

---
+++ src/WebAPI/Controllers/InternalController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers;
+
+[ApiController]
+[Route("api/internal")]
+public class InternalController : ControllerBase
+{
+    [HttpPost("judge-result")]
+    public IActionResult Receive([FromBody] object data)
+    {
+        Console.WriteLine(data);
+        return Ok("received");
+    }
+
+    [HttpGet("health")]
+    public IActionResult Health()
+    {
+        return Ok("ok");
+    }
+}

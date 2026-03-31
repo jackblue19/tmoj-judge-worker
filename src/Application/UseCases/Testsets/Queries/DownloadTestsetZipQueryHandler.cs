504e2615efa1290ba15787ed8416913b378fffc2
Jack Blue <git.jack1624@gmail.com>
2026-03-31 15:19:19 +0700

    test: unactive auth for download

--- src/Application/UseCases/Testsets/Queries/DownloadTestsetZipQueryHandler.cs
+++ src/Application/UseCases/Testsets/Queries/DownloadTestsetZipQueryHandler.cs
@@ -53,13 +53,13 @@ public sealed class DownloadTestsetZipQueryHandler
         if ( problem is null )
             throw new KeyNotFoundException("Problem not found.");
 
-        var isInternal = _httpContextAccessor.HttpContext?.Items["IsInternal"] as bool? ?? false;
+        //var isInternal = _httpContextAccessor.HttpContext?.Items["IsInternal"] as bool? ?? false;
 
-        if ( !isInternal )
-        {
-            EnsureAuthenticated();
-            EnsureCanManageProblem(problem);
-        }
+        //if ( !isInternal )
+        //{
+        //    EnsureAuthenticated();
+        //    EnsureCanManageProblem(problem);
+        //}
         //EnsureCanManageProblem(problem);
 
         var testset = await _testsetReadRepository.GetByIdAsync(request.TestsetId , ct);

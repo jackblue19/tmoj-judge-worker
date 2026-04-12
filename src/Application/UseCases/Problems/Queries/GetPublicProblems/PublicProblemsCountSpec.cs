26ae48a5ad2d9fa5157fcdbe4ccc40ef660dd7ca
Jack Blue <git.jack1624@gmail.com>
2026-04-12 13:23:25 +0700

    hotfix

--- src/Application/UseCases/Problems/Queries/GetPublicProblems/PublicProblemsCountSpec.cs
+++ src/Application/UseCases/Problems/Queries/GetPublicProblems/PublicProblemsCountSpec.cs
@@ -9,8 +9,8 @@ public sealed class PublicProblemsCountSpec : Specification<Problem>
     {
         Query.Where(x =>
             x.IsActive &&
-            x.StatusCode == "published" &&
-            x.StatusCode == "draft" &&
+            (x.StatusCode == "published" ||
+            x.StatusCode == "draft") &&
             x.VisibilityCode == "public");
 
         if ( !string.IsNullOrWhiteSpace(search) )

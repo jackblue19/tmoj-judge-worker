6c14c145636774fa65b0e094116731e6298c8bd2
Jack Blue <git.jack1624@gmail.com>
2026-04-12 12:27:26 +0700

    func: get all personal submitted problems /api/v2/users/problems

---
+++ src/Application/UseCases/Problems/Queries/GetProblemsByUser/IUserProblemQueries.cs
@@ -0,0 +1,13 @@
+﻿using Application.Common.Pagination;
+
+namespace Application.UseCases.Problems.Queries.GetProblemsByUser;
+
+public interface IUserProblemQueries
+{
+    Task<ApiPagedResponse<ProblemByUserListItemDto>> GetProblemsByUserAsync(
+        Guid userId ,
+        int page ,
+        int pageSize ,
+        string? traceId ,
+        CancellationToken ct = default);
+}
---
+++ src/Application/UseCases/Problems/Queries/GetProblemsByUser/ProblemByUserListItemDto.cs
@@ -0,0 +1,14 @@
+﻿namespace Application.UseCases.Problems.Queries.GetProblemsByUser;
+
+public sealed record ProblemByUserListItemDto(
+    Guid ProblemId ,
+    string Slug ,
+    string Title ,
+    string? Difficulty ,
+    string? TypeCode ,
+    bool IsActive ,
+    int Attempts ,
+    bool Solved ,
+    Guid? BestSubmissionId ,
+    DateTime? LastSubmissionAt
+);

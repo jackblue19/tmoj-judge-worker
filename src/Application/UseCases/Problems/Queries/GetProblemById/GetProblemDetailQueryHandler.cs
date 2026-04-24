1181f8c82fcabc306cd611c9c2cb6fe56e1e2b31
Jack Blue <git.jack1624@gmail.com>
2026-04-24 10:00:35 +0700

    fix

--- src/Application/UseCases/Problems/Queries/GetProblemById/GetProblemDetailQueryHandler.cs
+++ src/Application/UseCases/Problems/Queries/GetProblemById/GetProblemDetailQueryHandler.cs
@@ -27,8 +27,7 @@ public sealed class GetProblemDetailQueryHandler
         CancellationToken ct)
     {
         var publicResult = await _readRepository.FirstOrDefaultAsync(
-            new ProblemPublicDetailSpec(request.ProblemId) ,
-            ct);
+            new ProblemPublicDetailSpec(request.ProblemId) , ct);
 
         if ( publicResult is not null )
             return publicResult;
@@ -38,7 +37,8 @@ public sealed class GetProblemDetailQueryHandler
 
         var currentUserId = _currentUser.UserId.Value;
 
-        var isAdmin = Roles.AdminRoles.Any(role => _currentUser.IsInRole(role));
+        var isAdmin = Roles.AdminRoles
+            .Any(role => _currentUser.IsInRole(role) || _currentUser.IsInRole("student"));
 
         var managementResult = await _readRepository.FirstOrDefaultAsync(
             new ProblemManagementDetailSpec(
